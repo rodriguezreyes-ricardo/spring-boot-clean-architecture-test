8d90ff90cd53644511bab84ffcdd0955f683dc06
rodriguezreyes-ricardo <42134012+rodriguezreyes-ricardo@users.noreply.github.com>
2024-07-31 16:10:41 +0200

    Update ProductPricesControllerAdvice.java

--- src/main/java/com/testjava/productprices/adapter/in/web/ProductPricesControllerAdvice.java
+++ src/main/java/com/testjava/productprices/adapter/in/web/ProductPricesControllerAdvice.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.testjava.productprices.domain.ExceptionPrice;
 import com.testjava.productprices.domain.Price;
 
 /**
@@ -22,7 +24,7 @@ public class ProductPricesControllerAdvice {
 	 */
 	@SuppressWarnings("rawtypes")
 	@ExceptionHandler({ IllegalArgumentException.class })
-	public ResponseEntity priceExceptionHandle(Exception ex) {
+	public ResponseEntity priceBadRequestExceptionHandle(Exception ex) {
 		if (ex.getCause() instanceof ConversionFailedException) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Price.INCORRECT_DATE_FORMAT);
 		} else if (ex.getCause() instanceof NumberFormatException) {
@@ -30,6 +32,30 @@ public class ProductPricesControllerAdvice {
 		}
 
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+	/**
+	 * Price not found exception handle.
+	 *
+	 * @param ex the ex
+	 * @return the response entity
+	 */
+	@SuppressWarnings("rawtypes")
+	@ExceptionHandler({ NoHandlerFoundException.class })
+	public ResponseEntity priceNotFoundExceptionHandle(Exception ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Price.NOT_FOUND);
+	}
+
+	/**
+	 * Price internal server exception handle.
+	 *
+	 * @param ex the ex
+	 * @return the response entity
+	 */
+	@SuppressWarnings("rawtypes")
+	@ExceptionHandler({ ExceptionPrice.class })
+	public ResponseEntity priceInternalServerExceptionHandle(ExceptionPrice ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Price.INTERNAL_SERVER_ERROR);
 	}
 
 }
