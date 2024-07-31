9ebe81da234138062b9c7668365761bb29190c7f
rodriguezreyes-ricardo <42134012+rodriguezreyes-ricardo@users.noreply.github.com>
2024-07-31 16:12:04 +0200

    Add files via upload

---
+++ src/main/java/com/testjava/productprices/domain/ExceptionPrice.java
@@ -0,0 +1,18 @@
+package com.testjava.productprices.domain;
+
+/**
+ * The Class ExceptionPrice.
+ */
+public class ExceptionPrice  extends Exception {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 7804662959179866123L;
+
+	/**
+	 * Instantiates a new exception price.
+	 */
+	public ExceptionPrice() {
+	      super();
+	  }
+
+}
