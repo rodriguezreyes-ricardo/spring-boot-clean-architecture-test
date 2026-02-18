81cf4d855773f4aaeec2841c49ee63f9fd6b0290
rodriguezreyes-ricardo <rodriguezreyes.ricardo@hotmail.com>
2026-02-19 00:03:41 +0100

    Prepare pom to war

--- src/main/java/com/testjava/productprices/ProductPricesApplication.java
+++ src/main/java/com/testjava/productprices/ProductPricesApplication.java
@@ -2,12 +2,13 @@ package com.testjava.productprices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The Class ProductPrices.
  */
 @SpringBootApplication
-public class ProductPricesApplication {
+public class ProductPricesApplication extends SpringBootServletInitializer{
 
 	/**
 	 * The main method.
